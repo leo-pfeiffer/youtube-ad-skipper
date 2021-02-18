17fa738720e25d57ef2460849bc0763f1c5785ae
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2021-02-18 14:08:51 +0000

    Added script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name YouTube Ad Skipper
+// @namespace https://github.com/leo-pfeiffer
+// @version 0.1
+// @description  Skip ads on youtube
+// @match *://www.youtube.com/*
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+(function() {
+
+	let checkInterval;
+
+	let resetInterval = function() {
+		if (checkInterval) {
+			clearTimeout(checkInterval);
+			checkInterval = setTimeout(skip, 500);
+		}
+		checkInterval = setTimeout(skip, 500)
+	}
+
+	let skip = function() {
+		let btn = document.querySelector(".ytp-ad-skip-button.ytp-button") ||
+			document.querySelector(".videoAdUiSkipButton");
+
+		if (btn) {
+			btn.click();
+			resetInterval();
+		}
+
+		else {
+			resetInterval();
+		}
+
+	}
+
+	skip();
+})();
